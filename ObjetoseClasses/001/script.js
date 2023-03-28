8907d8c4ff397d45c63b0dc0600a85a148b6166e
t-lira <119820325+t-lira@users.noreply.github.com>
2023-03-28 18:05:07 -0300

    Criando Objetos e Classes em JavaScript

---
+++ ObjetoseClasses/001/script.js
@@ -0,0 +1,44 @@
+
+/*2) Crie uma classe para representar pessoas.
+Para cada pessoa teremos os atributos nome, peso e altura.
+As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
+Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor
+do seu IMC;*/
+
+class pessoa {
+  nome;
+  peso;
+  altura;
+
+  constructor(nome, peso, altura) {
+    this.nome = nome;
+    this.peso = peso;
+    this.altura = altura;
+  }
+
+  calcularImc() {
+    return this.peso / (this.altura * this.altura);
+  }
+
+  classifiqueImc() {
+    const imc = this.calcularImc();
+
+    if (imc < 18.5) {
+      return "Abaixo do peso";
+    } else if (imc >= 18.5 && imc < 25) {
+      return "Peso normal";
+    } else if (imc >= 25 && imc < 30) {
+      return "Acima do peso";
+    } else if (imc >= 30 && imc < 40) {
+      return "Obeso";
+    } else {
+      return "Obsedidade Grave";
+    }
+  }
+}
+
+const jose = new pessoa("josé", 70, 1.75);
+const thais = new pessoa("thais", 73, 1.68);
+
+console.log(thais.classifiqueImc());
+console.log(jose.classifiqueImc());
