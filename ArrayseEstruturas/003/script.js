6228beece2387fa58f9f8bb74dbe76178c4fea77
t-lira <119820325+t-lira@users.noreply.github.com>
2023-03-28 20:30:11 -0300

    Arrays e Estruturas de Repetição em JavaScript

---
+++ ArrayseEstruturas/003/script.js
@@ -0,0 +1,23 @@
+/*Dado uma lista com as notas tiradas pelos alunos. imprima a maior nota.
+Ex de lista de notas = [2, 7, 3, 8, 10, 4]*/
+
+/*const notas = []
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+notas.push(8);
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+  const nota = notas[i];
+  soma = soma + nota;
+}
+
+const media = soma / notas.length
+
+console.log(soma)
+console.log(media)*/
